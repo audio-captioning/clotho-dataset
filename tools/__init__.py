0735f294c2626387b6d82f74f37367d7034123c0
konstantinosdrosos <konstantinos.drossos@tuni.fi>
2019-11-04 14:47:43 +0100

    Init file for the project's tools.

---
+++ tools/__init__.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import tools.argument_parsing
+import tools.aux_functions
+import tools.captions_functions
+import tools.csv_functions
+import tools.file_io
+import tools.printing
+import tools.yaml_loader
+
+__author__ = 'Konstantinos Drossos -- Tampere University'
+__docformat__ = 'reStructuredText'
+__all__ = [
+    'argument_parsing', 'aux_functions',
+    'captions_functions', 'csv_functions',
+    'file_io',
+    'printing',
+    'yaml_loader'
+]
+
+
+# EOF
