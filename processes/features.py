1fecb3f2dd2947bca462ab2b284720ccc2dbf800
konstantinosdrosos <konstantinos.drossos@tuni.fi>
2019-11-04 17:56:31 +0100

    Added file for feature extraction.

---
+++ processes/features.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# imports
+
+__author__ = 'Konstantinos Drossos -- Tampere University'
+__docformat__ = 'reStructuredText'
+__all__ = ['extract_features']
+
+
+def extract_features(settings):
+    pass
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
+
+# EOF
