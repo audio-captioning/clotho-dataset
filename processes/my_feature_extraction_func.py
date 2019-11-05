c71bb362035ae56e6f32731551ce9046a1f758fe
konstantinosdrosos <konstantinos.drossos@tuni.fi>
2019-11-05 11:50:59 +0100

    Removed the features section.

---
+++ processes/my_feature_extraction_func.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# imports
+
+__author__ = 'Konstantinos Drossos'
+__docformat__ = 'reStructuredText'
+__all__ = []
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
+
+# EOF
