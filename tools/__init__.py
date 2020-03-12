4276bc95477917ee22d7a502df61269cdba2e6ab
Konstantinos Drosos <kostas.drosos@icloud.com>
2020-03-12 12:32:38 +0200

    Renames aux_functions to dataset_functions.

--- tools/__init__.py
+++ tools/__init__.py
@@ -2,9 +2,9 @@
 # -*- coding: utf-8 -*-
 
 import tools.argument_parsing
-import tools.aux_functions
 import tools.captions_functions
 import tools.csv_functions
+import tools.dataset_functions
 import tools.file_io
 import tools.yaml_loader
 import tools.multi_processing
@@ -12,9 +12,13 @@ import tools.multi_processing
 __author__ = 'Konstantinos Drossos -- Tampere University'
 __docformat__ = 'reStructuredText'
 __all__ = [
-    'argument_parsing', 'aux_functions',
-    'captions_functions', 'csv_functions',
-    'file_io', 'yaml_loader', 'multi_processing'
+    'argument_parsing',
+    'captions_functions',
+    'csv_functions',
+    'dataset_functions',
+    'file_io',
+    'multi_processing',
+    'yaml_loader'
 ]
 
 
